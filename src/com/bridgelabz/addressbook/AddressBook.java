d787232a44103315d59b43de7a052b7a5a40cf96
shipramargurate <shivanikumari22322@gmail.com>
2022-10-26 00:28:46 +0530

    [shivani]: added functionality to delete existing contact in address program

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -39,4 +39,56 @@ public class AddressBook {
 		}
 
 	}
+
+	public void editContact() {
+
+		System.out.println("Enter the First Name of Contact to Edit : ");
+		String FirstName = (sc.next());
+		boolean IsAvaible = false;
+
+		for (Contact contact : contactArrayList) {
+			if (contact.getFirstName().equals(FirstName)) {
+				IsAvaible = true;
+
+				System.out.println("Enter the First Name : ");
+				contact.setFirstName(sc.next());
+				System.out.println("Enter the Last Name : ");
+				contact.setLastName(sc.next());
+				System.out.println("Enter the Address : ");
+				contact.setAddress(sc.next());
+				System.out.println("Enter the City : ");
+				contact.setCity(sc.next());
+				System.out.println("Enter the State : ");
+				contact.setState(sc.next());
+				System.out.println("Enter the Zipcode : ");
+				contact.setZipCode(sc.nextInt());
+				System.out.println("Enter the Mobile Number : ");
+				contact.setMobileNo(sc.nextInt());
+				System.out.println("Enter the Email Id : ");
+				contact.setEmailID(sc.next());
+			}
+		}
+
+		if (IsAvaible == false) {
+			System.out.println("CONTACT NOT AVAIBLE");
+		}
+	}
+
+	public void deleteContact() {
+		System.out.println("Enter the First Name to DELETE from address book : ");
+		String FirstName = (sc.next());
+		boolean IsAvaible = false;
+
+		for (Contact contact : contactArrayList) {
+			if (contact.getFirstName().equalsIgnoreCase(FirstName)) {
+				IsAvaible = true;
+				contactArrayList.remove(contact);
+				System.out.println("CONTACT IS DELETED");
+				break;
+			}
+		}
+		if (IsAvaible == false) {
+			System.out.println("CONTACT NOT AVAIBLE");
+		}
+	}
 }
